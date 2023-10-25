5ac3e90ddb9c82f1164f93baf77803a1a1011af3
Sudongk <aa88sd@naver.com>
2023-10-25 19:22:34 +0900

    init setup

---
+++ src/Router.js
@@ -0,0 +1,62 @@
+const router = createBrowserRouter([
+
+    // {
+    //   path : '/',
+    //   element : <App />,
+    // },
+    // {
+    //   path : '/signUp',
+    //   element : <Signup />,
+    // },
+    // {
+    //   path : '/category',
+    //   element : <UserCategoryType />,
+    // },
+    // {
+    //   path : '/main',
+    //   element : <Main />,
+    // },
+    // {
+    //   path : '/myClass',
+    //   element : <MyClass />,
+    // },
+    // {
+    //   path : '/myPage',
+    //   element : <MyPage />,
+    // },
+    // {
+    //   path : '/classDetail',
+    //   element : <ClassDetail />,
+    // },
+    // {
+    //   path : '/auth',
+    //   element : <TeacherMiddleWare />,
+    // },
+    // {
+    //   path : '/authSignup',
+    //   element : <TeacherSignup />,
+    // },
+    // {
+    //   path : '/createCommunity',
+    //   element : <CreateCommunity />,
+    // },
+    // {
+    //   path : '/createSchedule',
+    //   element : <CreateSchedule />,
+    // },
+    // {
+    //   path : '/createBoard',
+    //   element : <CreateBoard />,
+    // },
+    // {
+    //   path : '/createAlbum',
+    //   element : <CreateAlbum />,
+    // },
+    // {
+    //   path : '/modifyInfo',
+    //   element : <ModifyInfo />,
+    // },
+
+]);
+
+export default router;

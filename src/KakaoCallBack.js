1e6ec93a8da806976743fdd9b3afba4a69bf3945
HeeHeeg <qmffndoden@naver.com>
2023-11-21 14:29:15 +0900

    로그인 스피너

--- src/KakaoCallBack.js
+++ src/KakaoCallBack.js
@@ -1,10 +1,9 @@
-import {login} from "./common/api/ApiGetService";
 import {useEffect} from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Center from "./components/Center";
+import './CallBack.css'
+
 const KakaoCallBack = () => {
-    // const code = new URL(window.location.href).searchParams.get("code");
-    // const navigate = useNavigate();
 
     useEffect(() => {
         // URL에서 code 파라미터 추출
@@ -33,11 +32,14 @@ const KakaoCallBack = () => {
 
     return (
         <div className="LoginHandeler">
-            <div className="notice">
-                <p>로그인 중입니다.</p>
-                <p>잠시만 기다려주세요.</p>
-                <div className="spinner"></div>
-            </div>
+            <Center>
+                <div className="loding">
+                    <p>로그인 중입니다.</p>
+                    <p>잠시만 기다려주세요.</p>
+                    <div className="spinner"></div>
+
+                </div>
+            </Center>
         </div>
     );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,23 +1,9 @@
 import login from './img/kakao_login.png'
-import {useEffect} from "react";
-import axios from "axios";
 
 const Login = () => {
     const CLIENT_ID = 'dd83fb5281e50c8508ffc20b8dc07799'
     const REDIRECT_URI = 'http://localhost:3000/oauth2/callback/kakao'
     const kakao_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`
-
-    // const fetchProtectedData = async () => {
-    //     try {
-    //         const accessToken = localStorage.getItem('accessToken');
-    //         const res = await axios.get({kakao_URL});
-    //         // 받은 데이터 처리
-    //         console.log(res.data);
-    //     } catch (error) {
-    //         console.error("데이터 요청 오류", error);
-    //         // 토큰 만료나 인증 오류 처리
-    //     }
-    // }
 
 
     return (

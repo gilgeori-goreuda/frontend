cb4ab21bc25c56e4c5e1cc2f1970040c7123826f
HeeHeeg <qmffndoden@naver.com>
2023-11-22 19:56:50 +0900

    로그인 css

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,6 @@
 import login from './img/kakao_login.png'
 import logo from './img/logo.png'
-import loginBorder from './img/loginBorder.jpg';
+import loginBorder from './img/loginBorder3.jpg';
 
 const Login = () => {
     const CLIENT_ID = 'dd83fb5281e50c8508ffc20b8dc07799'
@@ -17,16 +17,25 @@ const Login = () => {
                 justifyContent: 'center',
                 height: '100vh', // 전체 화면 높이
                 border: '8px solid transparent', // 테두리 크기 설정
-                borderImage: `url(${loginBorder}) 30 round` // 테두리 이미지 설정
+                backgroundImage: `url(${loginBorder})`, // 테두리 이미지 설정
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                position: 'relative'
             }}>
-                <div>
-                    <img src={logo} style={{width: '200px', height: 'auto'}}/>
+                <div style={{background: '#fff', width: '80%', height: '70%', position: 'absolute', top : '50%', left: '50%', transform: 'translate(-50%, -55%)', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <div>
+                        <div>
+                            <img src={logo} style={{width: '200px', height: 'auto'}}/>
+                        </div>
+                        <div style={{margin: '30px'}}></div>
+                        <div>
+                            <img src={login} onClick={() => window.location.href = kakao_URL}/>
+                        </div>
+                        <div style={{margin: '30px'}}></div>
+                    </div>
+
                 </div>
-                <div style={{margin: '30px'}}></div>
-                <div>
-                    <img src={login} onClick={() => window.location.href = kakao_URL}/>
-                </div>
-                <div style={{margin: '30px'}}></div>
+
             </div>
         </div>
     )

78faf79519c2aeeaeceab1dacd0e4aab9e0af813
HeeHeeg <qmffndoden@naver.com>
2023-11-23 20:40:25 +0900

    StoreDetail-css

--- src/Router.js
+++ src/Router.js
@@ -64,7 +64,7 @@ const router = createBrowserRouter([
       },
 
       {
-        path: '/StoreDetail',
+        path: '/StoreDetail/:storeId',
         element: <StoreDetail />,
       },
       {

c6043943ac474a2243d3aae071918d2cb648d723
alsensto <jmg1301@gmail.com>
2023-11-24 17:08:29 +0900

    feature/main

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,16 +1,29 @@
 import MainService from "./MainService";
 import StaticService from "./StaticService";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import Slider from "react-slick"
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './Main.css'
 import {Link} from "react-router-dom";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './swiperStyle.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
 const Main = () => {
 
     const settings = {
-        dots: true,
+        dots: false,
+        arrow: false,
+        fade: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
@@ -75,22 +88,51 @@ const Main = () => {
 
 
     return (
-        <div className={"main"}>
-
+        <div>
             <h2> 오늘의 신규 장소 </h2>
             <div className={"place"}>
+                <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+                    {newPlaces && newPlaces.map((newPlace, index) => (
+                        <div key={index}>
+                            <SwiperSlide>
+                                <div style={{display : 'flex', alignItems: 'center', flexDirection : 'column', gap : '20px', width: '100%'}}>
+                                    <div style={{width: '90%', height: '35vh'}}>
+                                        <Link to={`http://localhost:3000/storeDetail/${newPlace.id}`}>
+                                            <img style={{borderRadius: '10px'}} src={newPlace.imageUrl} alt={newPlace.name}/>
+                                        </Link>
+                                    </div>
+                                    <div className="main-details" style={{width: '50vh', display: 'flex', flexDirection:'column', gap: '5px'}}>
+                                        <p style={{margin : '0', padding: '0', fontSize: '15px'}}>가게 이름 : {newPlace.name}</p>
+                                        <p style={{margin : '0', padding: '0', fontSize: '15px'}}>주소 : {newPlace.streetAddress}</p>
+                                        <p style={{margin : '0', padding: '0', fontSize: '15px'}}>영업시간 : {newPlace.openTime} - {newPlace.closeTime}</p>
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+
+                        </div>
+                    ))}
+                </Swiper>
                 <Slider {...settings}>
                     {newPlaces && newPlaces.map((newPlace, index) => (
                         <div key={index}>
-                            <Link to={`http://localhost:3000/storeDetail/${newPlace.id}`}>
-                                <img src={newPlace.imageUrl} alt={newPlace.name}/>
-                            </Link>
-                            <div>{newPlace.name}</div>
-                            <div>{newPlace.openTime} ~ {newPlace.closeTime}</div>
-                            <p>streetAddr: {newPlace.streetAddress}</p>
-                            <p>detailAddr: {newPlace.detailLocation}</p>
+                            <div className="main-image">
+                                <Link to={`http://localhost:3000/storeDetail/${newPlace.id}`}>
+                                    <img src={newPlace.imageUrl} alt={newPlace.name}/>
+                                </Link>
+                            </div>
+                            <div className="main-details">
+                                <h3>상세정보</h3>
+                                <div style={{margin: '20px'}}></div>
+                                <div>가게 이름 : {newPlace.name}</div>
+                                <div style={{margin: '20px'}}></div>
+                                <div>주소 : {newPlace.streetAddress}</div>
+                                <div style={{margin: '20px'}}></div>
+                                <div>영업시간 : {newPlace.openTime} - {newPlace.closeTime}</div>
+                                <div style={{margin: '20px'}}></div>
+                            </div>
                         </div>
                     ))}
+
                 </Slider>
             </div>
 

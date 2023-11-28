42902da64d9e4d48df127472337e749b30c1142e
jun <dudwns1852@naver.com>
2023-11-28 10:18:21 +0900

    url 수정

--- src/components/main/StaticService.js
+++ src/components/main/StaticService.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://ec2-43-201-35-43.ap-northeast-2.compute.amazonaws.com:8080/api/v1/hotplaces';
+const API_BASE_URL = 'http://3.38.251.85/api/v1/hotplaces';
 const StaticService = {
-    getTop10Place:() => {
+    getTop10Place: () => {
         return axios.get(`${API_BASE_URL}`);
     }
 }
